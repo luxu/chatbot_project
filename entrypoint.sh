5bb28a5cadf80ab1366b8b05055cb25de9a5862d
Luciano Martins <luciano.martins@quay.com.br>
2026-01-31 11:22:00 -0300

    Add file: entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Ativa o ambiente virtual criado pelo 'uv'
+# Isso adiciona o diretório .venv/bin ao PATH do shell
+. /app/.venv/bin/activate
+
+# Aguardar PostgreSQL estar disponível
+echo "Waiting for external database at ${DB_HOST}:${DB_PORT}..."
+# wait-for-it db:5432 --timeout=60 --strict
+# wait-for-it ${DB_HOST}:${DB_PORT} --timeout=60 --strict -- echo "Database is ready!"
+
+# Executar migrações
+# echo "Running migrations..."
+# python manage.py migrate --noinput
+
+# Coleta todos os arquivos estáticos para a pasta STATIC_ROOT
+echo "Collecting static files..."
+python manage.py collectstatic --no-input --clear
+echo "Static files collected."
+# --no-input: Para não pedir confirmação (essencial para scripts)
+# --clear: Para limpar a pasta de estáticos antes de copiar os novos arquivos
+
+echo "Starting Django server..."
+exec "$@"
